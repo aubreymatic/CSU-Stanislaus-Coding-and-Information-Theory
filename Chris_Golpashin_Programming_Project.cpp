83c960b91ead1f14e324473ed0c5f394920866cc
Chris Golpashin <chrisromic@gmail.com>
2019-10-22 20:00:27 -0700

    Adding the starting file for project.

---
+++ Chris_Golpashin_Programming_Project.cpp
@@ -0,0 +1,14 @@
+// Chris Golpashin
+// Coding and Information Theory
+// Programming Project
+// Due December 3 2019
+
+#include <iostream>
+//using namespace std; // People on the internet say using namespace std is bad so yea ok then.
+
+int main()
+{
+    
+    
+    return 0;
+}
